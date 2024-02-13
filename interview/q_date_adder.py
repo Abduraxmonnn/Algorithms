f2e847bf467b2b56ff14b46c3e6c6a21b684f7c3
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2024-02-13 20:58:59 +0500

    updated date_adder algorithm

--- interview/q_date_adder.py
+++ interview/q_date_adder.py
@@ -2,34 +2,34 @@ import datetime
 from typing import Any
 
 
-# BigO(n^2)
+# BigO(n)
 def date_adder(date: Any, diapason: int, periods: int) -> list:
     converted_date = datetime.datetime.strptime(str(date), "%Y-%m-%d")
-    pre_result = [date]
+    result = [date]
     months = dict()
     months[int(date.split('-')[1])] = 1
-    for i in range(periods - 1):
-        added_date = converted_date + datetime.timedelta(days=diapason * (i + 1))
+
+    for item in range(periods - 1):
+        added_date = converted_date + datetime.timedelta(days=diapason)
         formatted_date = added_date.strftime("%Y-%m-%d")
-        current_iter = int(formatted_date.split('-')[1])
-        tmp = months.get(current_iter) if months.get(current_iter) else 0
-        months[current_iter] = 0
+        current_month = int(formatted_date.split('-')[1])
 
-        for key, value in months.items():
-            if key == current_iter:
-                if tmp < 2:
-                    months[key] = tmp + 1
-                    pre_result.append(formatted_date)
-                else:
-                    pre_result.append(added_date.replace(day=1, month=(added_date.month + 1)).strftime("%Y-%m-%d"))
-                    added_date = added_date.replace(day=1, month=(added_date.month + 1)).strftime("%Y-%m-%d")
-            else:
-                if tmp == 1:
-                    return pre_result
+        if months.get(current_month) is None:
+            result.append(formatted_date)
+            return result
 
-    return pre_result
+        if months.get(current_month) < 2:
+            months[current_month] = months.get(current_month) + 1
+            result.append(formatted_date)
+        else:
+            converted_date = added_date.replace(day=1, month=(added_date.month + 1))
+            result.append(converted_date.strftime("%Y-%m-%d"))
+            months[int(converted_date.strftime("%Y-%m-%d").split('-')[1])] = 1
+
+    return result
 
 
 res = date_adder("2024-01-01", 15, 6)
 # res = date_adder("2024-01-20", 15, 6)
+# res = date_adder("2024-01-17", 15, 6)
 print(res)
--- robocontest/test_3.py
+++
@@ -1,6 +0,0 @@
-# res = {"01": 0}
-res = {}
-for item in range(1, 10):
-    res[item] = 1
-
-print(res)
