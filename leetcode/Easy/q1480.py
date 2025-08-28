2413300c382d42bb6c6e89a555435e572e70772e
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2025-08-28 21:14:07 +0500

    modified(leetcode): q1480

--- leetcode/Easy/q1480.py
+++ leetcode/Easy/q1480.py
@@ -1,16 +1,26 @@
 class Solution(object):
     def runningSum(self, nums):
+        # -> 1
         for a in range(1, len(nums)):
             nums[a] += nums[a - 1]
         return nums
+
+        # -> 2
+        # step = 0
+        # result = []
+        #
+        # for i in nums:
+        #     step += i
+        #     result.append(step)
+        #
+        # return result
 
 
 if __name__ == '__main__':
     nums = [3, 1, 2, 10, 1]
     print(Solution().runningSum(nums))
 
-
 # 4chi qator / index boylab yurib 1ci indexga undan olldingi indexdagi soni qowaman (nums[a-1])
 # len() функция возвращает количество элементов объекта.
-# Отсутствие передачи аргумента или передача недопустимого аргумента вызовет TypeErrorисключение.
+# Отсутствие передачи аргумента или передача недопустимого аргумента вызовет TypeError исключение.
 # https://leetcode.com/problems/running-sum-of-1d-array/
