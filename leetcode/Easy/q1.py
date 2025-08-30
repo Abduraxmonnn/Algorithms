703f91c9797169a4f269e9f6c3efbde8345220e4
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2025-08-30 17:25:09 +0500

    modified(leetcode): question - 1

--- leetcode/Easy/q1.py
+++ leetcode/Easy/q1.py
@@ -1,16 +1,22 @@
 class Solution(object):
     def twoSum(self, nums, target):
+        # -> 1
         for i in range(len(nums)):
-            for a in range(i + 1, len(nums)):
-                if nums[i] + nums[a] == target:
-                    return i, a
+            a = target - nums[i]
+            if a in nums and i != nums.index(a):
+                return [i, nums.index(a)]
+
+        # # -> 2
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
 
 
 if __name__ == '__main__':
     nums = [3, 2, 4]
     target = 6
     print(Solution().twoSum(nums, target))
-
 
 # ( for a in range(i + 1, len(nums)): ) " i "dan 1ci indexdagi soni yoki holagan indexdagi soni oladi ca
 # " a "da " i " digidan 1ta index oldinda yuradi. Chunki bular " range() "ning ichiga yozilvoti.
