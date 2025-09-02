508e99ad1404d98b94d556bddfecb32d9d6e3563
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2025-09-02 09:51:16 +0500

    modified(algorithm): binary search

--- methods/binary_search.py
+++ methods/binary_search.py
@@ -1,44 +1,45 @@
-# Iterative
-def binary_search(array, x):
-    low = 0
-    high = len(array) - 1
-
-    while low <= high:
-
-        mid = low + (high - low) // 2
-
-        if array[mid] == x:
-            return array[mid]
-        elif array[mid] < x:
-            low = mid + 1
-        else:
-            high = mid - 1
-
-    return -1
-
-
-array = [3, 4, 5, 6, 7, 8, 9]
-x = 4
-
-result = binary_search(array, x)
-print(result)
-
-# # Recursive
-# def binary_search(array, low, high, x):
+# # Iterative
+# def binary_search(array, x):
+#     low = 0
+#     high = len(array) - 1
+#
 #     while low <= high:
 #
-#         mid = (high + low) // 2
+#         mid = low + (high - low) // 2
 #
 #         if array[mid] == x:
 #             return array[mid]
 #         elif array[mid] < x:
-#             return binary_search(array, mid + 1, high, x)
+#             low = mid + 1
 #         else:
-#             return binary_search(array, low, mid - 1, x)
-#
+#             high = mid - 1
 #
 #     return -1
 #
 #
-# result = binary_search(array,0, len(array) - 1, x)
+# array = [3, 4, 5, 6, 7, 8, 9]
+# x = 4
+#
+# result = binary_search(array, x)
 # print(result)
+
+# # Recursive
+def binary_search(array, low, high, x):
+    while low <= high:
+
+        mid = (high + low) // 2
+
+        if array[mid] == x:
+            return array[mid]
+        elif array[mid] < x:
+            return binary_search(array, mid + 1, high, x)
+        else:
+            return binary_search(array, low, mid - 1, x)
+
+
+    return -1
+
+array = [3, 4, 5, 6, 7, 8, 9]
+x = 10
+result = binary_search(array,0, len(array) - 1, x)
+print(result)
