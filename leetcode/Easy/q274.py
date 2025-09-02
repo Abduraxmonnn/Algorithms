14f5e052f8a14216f91c917c402e0b5b83dff1e6
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2025-09-02 09:51:00 +0500

    modified(leetcode): M question - 274 & 1528

--- leetcode/Easy/q274.py
+++ leetcode/Easy/q274.py
@@ -62,6 +62,26 @@ class Solution:
         #         return n - i
         # return 0
 
+        # # -> 4
+        # # O(n)
+        # citation_counter_length = len(citations) + 1  # 6
+        # h_index_count = [0] * citation_counter_length  # [0, 0, 0, 0, 0, 0]
+        #
+        # for i, val in enumerate(citations):
+        #     if citations[i] > citation_counter_length - 1:
+        #         h_index_count[citation_counter_length - 1] += 1
+        #     else:
+        #         h_index_count[citations[i]] += 1
+        #
+        # num_of_cit = 0
+        # for possible_h_index in range(citation_counter_length - 1, -1, -1):
+        #     num_of_cit += h_index_count[possible_h_index]
+        #
+        #     if num_of_cit >= possible_h_index:
+        #         return possible_h_index
+        #
+        # return h_index_count
+
 
 if __name__ == '__main__':
     citations = [3, 0, 6, 1, 5]  # 3
@@ -73,3 +93,5 @@ if __name__ == '__main__':
 #
 #
 # https://leetcode.com/problems/h-index/
+# 1. https://www.youtube.com/watch?v=z2Y0S9SfzEs
+# 2. https://www.youtube.com/watch?v=K_7OCBmzeRw
