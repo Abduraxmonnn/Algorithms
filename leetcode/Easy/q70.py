e882d4ca0830e3ea291ee07d284a8b84f8b168ce
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2025-09-15 10:23:41 +0500

    modified(leetcode): question - 70

--- leetcode/Easy/q70.py
+++ leetcode/Easy/q70.py
@@ -1,22 +1,32 @@
 class Solution(object):
     def climbStairs(self, n):
-        step_1 = 1
-        step_2 = 2
-        way = 0
+        ...
+        # -> 1
+        # Time Comp: O(N)
+        if n < 3:
+            return n
 
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
+        st1, st2 = 1, 2
+        for _ in range(n - 2):
+            st1, st2 = st2, st2 + st1
+        return st2
 
-        i = 2
-        while i < n:
-            way = step_1 + step_2
-            step_1 = step_2
-            step_2 = way
-            i = i + 1
-        return way
+        # # -> 2
+        # if n < 3:
+        #     return n
+        #
+        # sts = [1, 2]
+        # for i in range(2, n):
+        #     sts.append(sts[i-2] + sts[i-1])
+        # return sts[-1]
 
+    # # -> 3
+    # step = {1: 1, 2: 2}
+    # def climbStairs(self, n: int) -> int:
+    #     if n in self.step:
+    #         return self.step[n]
+    #     self.step[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+    #     return self.step[n]
 
 if __name__ == '__main__':
     n = 6
@@ -25,3 +35,6 @@ if __name__ == '__main__':
 #
 #
 # https://leetcode.com/problems/climbing-stairs/
+
+# Explanation 1: https://www.youtube.com/watch?v=u43aQ4Vgvs0
+# Explanation 2: https://www.youtube.com/shorts/UY6d4cv-0RI
