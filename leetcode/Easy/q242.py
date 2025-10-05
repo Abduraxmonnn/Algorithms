22103c39ef4e911a40d5b00f7ebbdcb682847f81
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2025-10-05 16:48:07 +0500

    modified(leetcode): question - 242

--- leetcode/Easy/q242.py
+++ leetcode/Easy/q242.py
@@ -1,22 +1,60 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        l_s = []
-        l_t = []
-
-        if len(s) == len(t):
-            c = 0
-            while len(s) != len(l_s):
-                l_s.append(s[c])
-                l_t.append(t[c])
-
-                c += 1
-        else:
+        # -> 1
+        # O(N)
+        if len(s) != len(t):
             return False
 
-        if sorted(l_s) == sorted(l_t):
-            return True
-        else:
-            return False
+        t_list = list(t)
+
+        for char in s:
+            if char not in t_list:
+                return False
+            t_list.remove(char)
+
+        return True
+
+        # # -> 2
+        # # O(N)
+        # if len(s) != len(t):
+        #     return False
+        #
+        # l_map = {}
+        # for char in t:
+        #     l_map[char] = l_map.get(char, 0) + 1
+        #
+        # for char in s:
+        #     if char not in t or l_map[char] == 0:
+        #         return False
+        #     l_map[char] -= 1
+        # return True
+
+        # # -> 3
+        # # O(n log n)
+        # if sorted(list(s)) == sorted(list(t)):
+        #     return True
+        # return False
+        # 2.2
+        # return Counter(s) == Counter(t)
+
+        # # OLD
+        # l_s = []
+        # l_t = []
+        #
+        # if len(s) == len(t):
+        #     c = 0
+        #     while len(s) != len(l_s):
+        #         l_s.append(s[c])
+        #         l_t.append(t[c])
+        #
+        #         c += 1
+        # else:
+        #     return False
+        #
+        # if sorted(l_s) == sorted(l_t):
+        #     return True
+        # else:
+        #     return False
 
 
 if __name__ == '__main__':
@@ -24,21 +62,6 @@ if __name__ == '__main__':
     t = "ab"
     print(Solution().isAnagram(s, t))
 
-
 #
 #
 # https://leetcode.com/problems/valid-anagram/
-
-# l_s = []
-# l_t = []
-#
-# for i in range(0, len(s)):
-#     l_s.append(s[i])
-#
-# for j in range(0, len(t)):
-#     l_t.append(t[j])
-#
-# if sorted(l_s) == sorted(l_t):
-#     return True
-# else:
-#     return False
