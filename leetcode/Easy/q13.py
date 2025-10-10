2fcdf430af39938d35e0c3cca3b8e221c854d80a
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2025-10-10 22:11:18 +0500

    modified(leetcode): question - 13

--- leetcode/Easy/q13.py
+++ leetcode/Easy/q13.py
@@ -1,23 +1,46 @@
 class Solution(object):
     def romanToInt(self, s):
-        numbers = {'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900,
-                   'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
-                   }
-        sum = 0
+        # -> 1
+        # TC: O(N)
+        # SC: o(1)
+        symbols = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        prev = 0
 
-        past = s[-1]
-
-        for n in list(s[::-1]):
-            if numbers[n] < numbers[past]:
-                sum = sum - numbers[n]
+        for i in s[::-1]:
+            if symbols[i] >= prev:
+                total += symbols[i]
             else:
-                sum = sum + numbers[n]
-                past = n
-        return (sum)
+                total -= symbols[i]
+            prev = symbols[i]
+
+        return total
+
+        # # -> 2
+        # # TC: O(N)
+        # # SC: o(1)
+        # symbols = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        # s_list = list(reversed(s))
+        # total = symbols[s_list[0]]
+        # prev = symbols[s_list[0]]
+        #
+        # for i in range(1, len(s_list)):
+        #     crt = s_list[i]
+        #     if symbols[crt] >= prev:
+        #         total += symbols[crt]
+        #         prev = symbols[crt]
+        #     else:
+        #         total -= symbols[crt]
+        #         prev = symbols[crt]
+        #
+        # return total
 
 
 if __name__ == '__main__':
-    s = "LVIII"
+    # s = "III"  # 3
+    # s = 'LVIII'  # 58
+    # s = 'MCMXCIV' # 1994
+    s = "IV"  # 4
     print(Solution().romanToInt(s))
 
 #
